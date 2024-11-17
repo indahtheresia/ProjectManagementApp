bbc7a8c3a4ba478a699eb640b78dc45c2569358f
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-11-17 12:28:42 +0800

    add style to ProjectSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 function App() {
   return (
-    <main>
+    <main className= "h-screen my-8">
       <ProjectsSidebar />
     </main>
   );
