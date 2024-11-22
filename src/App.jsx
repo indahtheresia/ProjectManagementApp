baaecdb1dddadd4a3368c91a53e0a8b27ad0dc4f
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-11-22 10:34:17 +0800

    handle project creation and update UI

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,14 @@ function App() {
 
   function handleAddProject(projectData){
     setProjectsState(prevState=>{
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       };
       return{
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     });
@@ -37,7 +39,7 @@ function App() {
   }
   return (
     <main className= "h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
